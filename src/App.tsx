1f11799872e54cf3c3e153344792ee43f684fb08
JithinPS <jithin.ps@ayatacommerce.com>
2024-05-11 16:24:42 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
